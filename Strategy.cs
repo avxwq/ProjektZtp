c46892613c2a937027f4f8c8b6c290c13b0e7877
lukasz <lukaszbondar10@gmail.com>
2025-01-26 19:34:04 +0100

    Poprawione Strategy

--- Strategy.cs
+++ Strategy.cs
@@ -86,7 +86,18 @@ namespace ProjektZtp
     {
         private HashSet<Position> hitPositions = new HashSet<Position>();
         private Queue<Position> shotQueue = new Queue<Position>();
-        private bool isHunting = false;
+        private List<Position> initialHitPositions = new List<Position>();
+        private bool isHorizontal = false;
+        private ShipHuntingState huntingState = ShipHuntingState.NotHunting;
+
+        private enum ShipHuntingState
+        {
+            NotHunting,
+            HuntingRight,
+            HuntingLeft,
+            HuntingUp,
+            HuntingDown
+        }
 
         public Tuple<Position, bool> GetShipPlacement(Board board, Ship ship)
         {
@@ -96,11 +107,11 @@ namespace ProjektZtp
             while (true)
             {
                 var startPosition = availablePositions[random.Next(availablePositions.Count)];
-                bool isHorizontal = random.Next(2) == 0;
+                bool shipHorizontal = random.Next(2) == 0;
 
-                if (board.CanPlaceShip(ship, startPosition, isHorizontal))
+                if (board.CanPlaceShip(ship, startPosition, shipHorizontal))
                 {
-                    return Tuple.Create(startPosition, isHorizontal);
+                    return Tuple.Create(startPosition, shipHorizontal);
                 }
             }
         }
@@ -112,13 +123,12 @@ namespace ProjektZtp
                 var position = shotQueue.Dequeue();
                 if (board.GetCell(position).Ship != null)
                 {
-                    AddAdjacentPositions(position, board);
-                    isHunting = true;
+                    initialHitPositions.Add(position);
+                    HandleHitShip(position, board);
                 }
-
                 else if (shotQueue.Count == 0)
                 {
-                    isHunting = false;
+                    UpdateHuntingStateAfterMiss();
                 }
                 return position;
             }
@@ -129,28 +139,157 @@ namespace ProjektZtp
             if (availablePositions.Count == 0)
                 throw new InvalidOperationException("All positions have already been shot at.");
 
-            var randomPosition = availablePositions[random.Next(availablePositions.Count)];
-            hitPositions.Add(randomPosition);
+            var selectedPosition = availablePositions[random.Next(availablePositions.Count)];
+            hitPositions.Add(selectedPosition);
 
-            if (board.GetCell(randomPosition).Ship != null)
+            if (board.GetCell(selectedPosition).Ship != null)
             {
-                AddAdjacentPositions(randomPosition, board);
-                isHunting = true;
+                initialHitPositions.Add(selectedPosition);
+                HandleHitShip(selectedPosition, board);
             }
 
+            return selectedPosition;
+        }
 
-            return randomPosition;
+        private void HandleHitShip(Position hitPosition, Board board)
+        {
+            if (initialHitPositions.Count == 2)
+            {
+                DetermineShipOrientation();
+                InitializeHuntingSequence(board);
+            }
+            else if (huntingState != ShipHuntingState.NotHunting)
+            {
+                ContinueHuntingSequence(board);
+            }
+            else
+            {
+                AddAdjacentPositions(hitPosition, board);
+            }
+        }
+
+        private void DetermineShipOrientation()
+        {
+            isHorizontal = initialHitPositions[0].X == initialHitPositions[1].X;
+        }
+
+        private void InitializeHuntingSequence(Board board)
+        {
+            if (isHorizontal)
+            {
+                huntingState = initialHitPositions[0].Y < initialHitPositions[1].Y
+                    ? ShipHuntingState.HuntingDown
+                    : ShipHuntingState.HuntingUp;
+            }
+            else
+            {
+                huntingState = initialHitPositions[0].X < initialHitPositions[1].X
+                    ? ShipHuntingState.HuntingRight
+                    : ShipHuntingState.HuntingLeft;
+            }
+            AddDirectionalPositions(board);
+        }
+
+        private void ContinueHuntingSequence(Board board)
+        {
+            switch (huntingState)
+            {
+                case ShipHuntingState.HuntingRight:
+                    TryHuntInDirection(board, new Position(1, 0), ShipHuntingState.HuntingLeft);
+                    break;
+                case ShipHuntingState.HuntingLeft:
+                    TryHuntInDirection(board, new Position(-1, 0), ShipHuntingState.NotHunting);
+                    break;
+                case ShipHuntingState.HuntingUp:
+                    TryHuntInDirection(board, new Position(0, -1), ShipHuntingState.HuntingDown);
+                    break;
+                case ShipHuntingState.HuntingDown:
+                    TryHuntInDirection(board, new Position(0, 1), ShipHuntingState.NotHunting);
+                    break;
+            }
+        }
+
+        private void TryHuntInDirection(Board board, Position direction, ShipHuntingState nextState)
+        {
+            var lastHit = initialHitPositions[initialHitPositions.Count - 1];
+            var nextPosition = new Position(lastHit.X + direction.X, lastHit.Y + direction.Y);
+
+            if (IsValidPosition(board, nextPosition) && !hitPositions.Contains(nextPosition))
+            {
+                shotQueue.Enqueue(nextPosition);
+                hitPositions.Add(nextPosition);
+            }
+            else
+            {
+                huntingState = nextState;
+            }
+        }
+
+        private void UpdateHuntingStateAfterMiss()
+        {
+            switch (huntingState)
+            {
+                case ShipHuntingState.HuntingRight:
+                    huntingState = ShipHuntingState.HuntingLeft;
+                    break;
+                case ShipHuntingState.HuntingLeft:
+                    huntingState = isHorizontal ? ShipHuntingState.NotHunting : ShipHuntingState.HuntingDown;
+                    break;
+                case ShipHuntingState.HuntingUp:
+                    huntingState = ShipHuntingState.HuntingDown;
+                    break;
+                case ShipHuntingState.HuntingDown:
+                    huntingState = !isHorizontal ? ShipHuntingState.NotHunting : ShipHuntingState.NotHunting;
+                    break;
+            }
+
+            if (huntingState == ShipHuntingState.NotHunting)
+            {
+                initialHitPositions.Clear();
+            }
+        }
+
+        private void AddDirectionalPositions(Board board)
+        {
+            var firstHit = initialHitPositions[0];
+            var secondHit = initialHitPositions[1];
+            var directions = GetSortedDirections(firstHit, secondHit);
+
+            foreach (var direction in directions)
+            {
+                if (IsValidPosition(board, direction) && !hitPositions.Contains(direction))
+                {
+                    shotQueue.Enqueue(direction);
+                    hitPositions.Add(direction);
+                }
+            }
+        }
+
+        private List<Position> GetSortedDirections(Position firstHit, Position secondHit)
+        {
+            if (isHorizontal)
+            {
+                return firstHit.Y < secondHit.Y
+                    ? new List<Position> { new Position(firstHit.X, firstHit.Y - 1), new Position(firstHit.X, secondHit.Y + 1) }
+                    : new List<Position> { new Position(firstHit.X, firstHit.Y + 1), new Position(firstHit.X, secondHit.Y - 1) };
+            }
+            else
+            {
+                return firstHit.X < secondHit.X
+                    ? new List<Position> { new Position(firstHit.X - 1, firstHit.Y), new Position(secondHit.X + 1, firstHit.Y) }
+                    : new List<Position> { new Position(firstHit.X + 1, firstHit.Y), new Position(secondHit.X - 1, firstHit.Y) };
+            }
         }
 
         private void AddAdjacentPositions(Position hitPosition, Board board)
         {
             var adjacentPositions = new List<Position>
-            {
-                new Position(hitPosition.X - 1, hitPosition.Y),
-                new Position(hitPosition.X + 1, hitPosition.Y),
-                new Position(hitPosition.X, hitPosition.Y - 1),
-                new Position(hitPosition.X, hitPosition.Y + 1)
-            };
+        {
+            new Position(hitPosition.X - 1, hitPosition.Y),
+            new Position(hitPosition.X + 1, hitPosition.Y),
+            new Position(hitPosition.X, hitPosition.Y - 1),
+            new Position(hitPosition.X, hitPosition.Y + 1)
+        };
 
             foreach (var adjPos in adjacentPositions)
             {
@@ -202,7 +341,18 @@ namespace ProjektZtp
     {
         private HashSet<Position> hitPositions = new HashSet<Position>();
         private Queue<Position> shotQueue = new Queue<Position>();
-        private bool isHunting = false;
+        private List<Position> initialHitPositions = new List<Position>();
+        private bool isHorizontal = false;
+        private ShipHuntingState huntingState = ShipHuntingState.NotHunting;
+
+        private enum ShipHuntingState
+        {
+            NotHunting,
+            HuntingRight,
+            HuntingLeft,
+            HuntingUp,
+            HuntingDown
+        }
 
         public Tuple<Position, bool> GetShipPlacement(Board board, Ship ship)
         {
@@ -212,11 +362,11 @@ namespace ProjektZtp
             while (true)
             {
                 var startPosition = availablePositions[random.Next(availablePositions.Count)];
-                bool isHorizontal = random.Next(2) == 0;
+                bool shipHorizontal = random.Next(2) == 0;
 
-                if (board.CanPlaceShip(ship, startPosition, isHorizontal))
+                if (board.CanPlaceShip(ship, startPosition, shipHorizontal))
                 {
-                    return Tuple.Create(startPosition, isHorizontal);
+                    return Tuple.Create(startPosition, shipHorizontal);
                 }
             }
         }
@@ -225,18 +375,17 @@ namespace ProjektZtp
         {
             if (shotQueue.Count > 0)
             {
-                var positions = shotQueue.Dequeue();
-                if (board.GetCell(positions).Ship != null)
+                var position = shotQueue.Dequeue();
+                if (board.GetCell(position).Ship != null)
                 {
-                    AddAdjacentPositions(positions, board);
-                    isHunting = true;
+                    initialHitPositions.Add(position);
+                    HandleHitShip(position, board);
                 }
-
                 else if (shotQueue.Count == 0)
                 {
-                    isHunting = false;
+                    UpdateHuntingStateAfterMiss();
                 }
-                return positions;
+                return position;
             }
 
             var availablePositions = GetGridShotPositions(board);
@@ -244,28 +393,149 @@ namespace ProjektZtp
             if (availablePositions.Count == 0)
                 throw new InvalidOperationException("All positions have already been shot at.");
 
-            var position = availablePositions.First();
-            hitPositions.Add(position);
+            var selectedPosition = availablePositions.First();
+            hitPositions.Add(selectedPosition);
 
-            if (board.GetCell(position).Ship != null)
+            if (board.GetCell(selectedPosition).Ship != null)
             {
-                AddAdjacentPositions(position, board);
-                isHunting = true;
+                initialHitPositions.Add(selectedPosition);
+                HandleHitShip(selectedPosition, board);
             }
 
+            return selectedPosition;
+        }
 
-            return position;
+        private void HandleHitShip(Position hitPosition, Board board)
+        {
+            if (initialHitPositions.Count == 2)
+            {
+                DetermineShipOrientation();
+                InitializeHuntingSequence(board);
+            }
+            else if (huntingState != ShipHuntingState.NotHunting)
+            {
+                ContinueHuntingSequence(board);
+            }
+            else
+            {
+                AddAdjacentPositions(hitPosition, board);
+            }
+        }
+
+        private void DetermineShipOrientation()
+        {
+            isHorizontal = initialHitPositions[0].X == initialHitPositions[1].X;
+        }
+
+        private void InitializeHuntingSequence(Board board)
+        {
+            if (isHorizontal)
+            {
+                huntingState = initialHitPositions[0].Y < initialHitPositions[1].Y
+                    ? ShipHuntingState.HuntingDown
+                    : ShipHuntingState.HuntingUp;
+            }
+            else
+            {
+                huntingState = initialHitPositions[0].X < initialHitPositions[1].X
+                    ? ShipHuntingState.HuntingRight
+                    : ShipHuntingState.HuntingLeft;
+            }
+            AddDirectionalPositions(board);
+        }
+
+        private void ContinueHuntingSequence(Board board)
+        {
+            switch (huntingState)
+            {
+                case ShipHuntingState.HuntingRight:
+                    TryHuntInDirection(board, new Position(1, 0), ShipHuntingState.HuntingLeft);
+                    break;
+                case ShipHuntingState.HuntingLeft:
+                    TryHuntInDirection(board, new Position(-1, 0), ShipHuntingState.NotHunting);
+                    break;
+                case ShipHuntingState.HuntingUp:
+                    TryHuntInDirection(board, new Position(0, -1), ShipHuntingState.HuntingDown);
+                    break;
+                case ShipHuntingState.HuntingDown:
+                    TryHuntInDirection(board, new Position(0, 1), ShipHuntingState.NotHunting);
+                    break;
+            }
+        }
+
+        private void TryHuntInDirection(Board board, Position direction, ShipHuntingState nextState)
+        {
+            var lastHit = initialHitPositions[initialHitPositions.Count - 1];
+            var nextPosition = new Position(lastHit.X + direction.X, lastHit.Y + direction.Y);
+
+            if (IsValidPosition(board, nextPosition) && !hitPositions.Contains(nextPosition))
+            {
+                shotQueue.Enqueue(nextPosition);
+                hitPositions.Add(nextPosition);
+            }
+            else
+            {
+                huntingState = nextState;
+            }
+        }
+
+        private void UpdateHuntingStateAfterMiss()
+        {
+            if (isHorizontal)
+            {
+                huntingState = huntingState == ShipHuntingState.HuntingDown
+                    ? ShipHuntingState.HuntingUp
+                    : ShipHuntingState.NotHunting;
+            }
+            else
+            {
+                huntingState = huntingState == ShipHuntingState.HuntingRight
+                    ? ShipHuntingState.HuntingLeft
+                    : ShipHuntingState.NotHunting;
+            }
+        }
+
+        private void AddDirectionalPositions(Board board)
+        {
+            var firstHit = initialHitPositions[0];
+            var secondHit = initialHitPositions[1];
+            var directions = GetSortedDirections(firstHit, secondHit);
+
+            foreach (var direction in directions)
+            {
+                if (IsValidPosition(board, direction) && !hitPositions.Contains(direction))
+                {
+                    shotQueue.Enqueue(direction);
+                    hitPositions.Add(direction);
+                }
+            }
+        }
+
+        private List<Position> GetSortedDirections(Position firstHit, Position secondHit)
+        {
+            if (isHorizontal)
+            {
+                return firstHit.Y < secondHit.Y
+                    ? new List<Position> { new Position(firstHit.X, firstHit.Y - 1), new Position(firstHit.X, secondHit.Y + 1) }
+                    : new List<Position> { new Position(firstHit.X, firstHit.Y + 1), new Position(firstHit.X, secondHit.Y - 1) };
+            }
+            else
+            {
+                return firstHit.X < secondHit.X
+                    ? new List<Position> { new Position(firstHit.X - 1, firstHit.Y), new Position(secondHit.X + 1, firstHit.Y) }
+                    : new List<Position> { new Position(firstHit.X + 1, firstHit.Y), new Position(secondHit.X - 1, firstHit.Y) };
+            }
         }
 
         private void AddAdjacentPositions(Position hitPosition, Board board)
         {
             var adjacentPositions = new List<Position>
-            {
-                new Position(hitPosition.X - 1, hitPosition.Y),
-                new Position(hitPosition.X + 1, hitPosition.Y),
-                new Position(hitPosition.X, hitPosition.Y - 1),
-                new Position(hitPosition.X, hitPosition.Y + 1)
-            };
+        {
+            new Position(hitPosition.X - 1, hitPosition.Y),
+            new Position(hitPosition.X + 1, hitPosition.Y),
+            new Position(hitPosition.X, hitPosition.Y - 1),
+            new Position(hitPosition.X, hitPosition.Y + 1)
+        };
 
             foreach (var adjPos in adjacentPositions)
             {
@@ -298,6 +568,7 @@ namespace ProjektZtp
             }
             return positions;
         }
+
         private List<Position> GetAllPositions(int boardSize)
         {
             var positions = new List<Position>();
